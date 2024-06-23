e943cf6e0562b69d6c29e8034664291ae699668e
U-HP-250-G7\raghh <raghhavdturki@gmail.com>
2024-06-23 11:30:50 +0530

    Add Token

---
+++ src/lib/token/token.ts
@@ -0,0 +1,55 @@
+export interface Token {
+  kind: TokenKind;
+  text: string;
+}
+
+export type TokenKind =
+  | "identifier"
+  | "integer"
+  | "string"
+  | "illegal"
+  | "eof"
+  | "assign"
+  | "plus"
+  | "minus"
+  | "bang"
+  | "asterisk"
+  | "slash"
+  | "lessThen"
+  | "greaterThen"
+  | "comma"
+  | "semicolon"
+  | "colon"
+  | "leftParenthesis"
+  | "rightParenthesis"
+  | "leftBrace"
+  | "rightBrace"
+  | "leftBracket"
+  | "rightBracket"
+  | "function"
+  | "let"
+  | "if"
+  | "else"
+  | "return"
+  | "true"
+  | "false"
+  | "equals"
+  | "notEquals";
+
+const keywords = new Map<string, TokenKind>([
+  ["fn", "function"],
+  ["let", "let"],
+  ["true", "true"],
+  ["false", "false"],
+  ["if", "if"],
+  ["else", "else"],
+  ["return", "return"],
+]);
+
+export const lookupIdentifier = (text: string): Token => {
+  const kind = keywords.get(text);
+  if (kind) {
+    return { kind, text };
+  }
+  return { kind: "identifier", text };
+};
